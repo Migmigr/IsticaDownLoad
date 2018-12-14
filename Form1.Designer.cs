c5bf555215e79bb8197109feea86d8c0c3a25b96
Migmigr <45515149+Migmigr@users.noreply.github.com>
2018-12-15 00:20:59 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Скачивание
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(454, 173);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(351, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "(Всего 73335 файлов)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Начать с";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(56, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(107, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Успех";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(234, 73);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(107, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(181, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ошибка";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Будет скачано 73335 файлов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 284);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Загрузка";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace Скачивание
+{
+    public partial class Form1 : Form
+    {
+        public int u;
+        public int n;
+        public int star;
+        public int kol;
+        BackgroundWorker[] BW;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            kol = 0;
+            star = Convert.ToInt32(textBox1.Text);
+
+            string path = Application.StartupPath + "\\Загрузки\\";
+            Directory.CreateDirectory(path);
+            string remoteUri = "http://ihtik.lib.ru/2011.07_ihtik_hudlit-ru/";
+
+            u = 0;
+            n = 0;
+            //73335
+
+            BW = new System.ComponentModel.BackgroundWorker[73335 - star];
+            for (int i = Convert.ToInt32(textBox1.Text); i <= 73335; i++)
+            {
+                int flag = 0;
+                foreach (Form frm in Application.OpenForms)
+                {
+                    if (frm.Name == "Form1")
+                        flag = 1;
+                }
+                if (flag == 0)
+                    break;
+                while (kol > 0)
+                    Application.DoEvents();
+
+                string fileName = "2011.07_ihtik_hudlit-ru_" + i + ".rar";
+                BW[i - star] = new BackgroundWorker();
+                BW[i - star].DoWork += new DoWorkEventHandler(BackgroundWorker1_DoWork);
+                BW[i - star].RunWorkerCompleted += new RunWorkerCompletedEventHandler(BackgroundWorker1_RunWorkerCompleted);
+                Downloading dd = new Downloading(remoteUri, fileName);
+                BW[i - star].RunWorkerAsync(dd);
+                kol++;
+            }
+            textBox1.Enabled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                label5.Text = "Будет скачано " +
+                  ((73335 - Convert.ToInt32(textBox1.Text) > 0) ?
+                  (73335 - Convert.ToInt32(textBox1.Text) + 1) : 0) +
+                    " файлов";
+            }
+        }
+
+        public class Downloading
+        {
+            public string dpath;
+            public string ad;
+            public string file;
+            public int u;
+
+            public Downloading(string ad, string file)
+            {
+                this.dpath = Application.StartupPath + "\\Загрузки\\" + file;
+                this.ad = ad + file;
+                this.file = file;
+            }
+            public int SD()
+            {
+                try
+                {
+                    WebClient myWebClient = new WebClient();
+                    myWebClient.DownloadFile(ad, dpath);
+                    u++;
+                }
+                catch (WebException ex)
+                {
+                    u--;
+                }
+                return u;
+            }
+        }
+        public struct resul
+        {
+            public int u;
+            public string file;
+        }
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Downloading D = (Downloading)e.Argument;
+            resul re = new resul();
+            re.u = D.SD();
+            re.file = D.file;
+            e.Result = re;
+        }
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            resul A = (resul)e.Result;
+            if (A.u > 0)
+            {
+                u++;
+                textBox2.Text = u + "(" + Math.Round((double)u / ((double)73335 - (double)star), 3) + "%)";
+                listBox1.Items.Add(A.file + "....Упех.");
+            }
+            else
+            {
+                n++;
+                textBox3.Text = n + "(" + Math.Round((double)n / ((double)73335 - (double)star), 3) + "%)";
+                listBox1.Items.Add(A.file + "....ОШИБКА.");
+            }
+            kol--;
+        }
+    }
+}
